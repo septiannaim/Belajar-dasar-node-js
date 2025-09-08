8e35024c01f4208869edc01f50864848a2a9b098
Septian Na'im <naimplung28@gmail.com>
2025-09-08 11:31:17 +0700

    update global

---
+++ global.js
@@ -0,0 +1,7 @@
+setTimeout( function() {
+    console.info("Function A executed");
+}, 3000);
+
+// Global adalah objek yang bersifat global
+// artnya bisa diakses dimanapun tanpa perlu mengimport atau require
+// contoh global adalah setTimeout, clearTimeout, setInterval, clearInterval, dll
